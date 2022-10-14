80e9e5c6766a530b7c1178e323af4645353fec5b
lanthierj <lanthierj@educ.college-montreal.qc.ca>
2022-10-14 08:54:28 -0400

    terminer le code

--- main.py
+++ main.py
@@ -2,25 +2,32 @@
 #TP2 jeu de la devinette
 import random
 
-chiffre = random.randint(1, 100)
+jouer = True
+chiffre = random.randint(0, 100)
 nbessaie = 0
 
-
-
-def start():
-    print('J ai choisi un nombre entre 0 et 100. À vous de le deviner....')
-    main()
-
-def main():
+while jouer:
     essai = int(input('Entrer votre essaie:'))
-    nbessaie + 1
+
     if essai < chiffre:
         print('Mauvaise réponse, le nombre est plus grand que', essai)
-        main()
-    if essai > chiffre:
-        print('Mauvaise réponse, le nombre est plus grand que')
-        main()
-    if essai == chiffre:
-        print('Bravo! Bonne réponse/Vous avez réussi en : ')
+        nbessaie += 1
 
-start()
+
+    elif essai > chiffre:
+        print('Mauvaise réponse, le nombre est plus petit que', essai)
+        nbessaie += 1
+
+
+    elif essai == chiffre:
+        print('Bravo! Bonne réponse/Vous avez réussi en : ', nbessaie, 'essaie')
+        restart = input('Voulez-vous faire une autre partie (o,n)?')
+
+        if restart == 'n':
+            print('Merci et au revoir... ')
+            jouer = False
+
+        elif restart == 'o':
+            chiffre = random.randint(0, 10)
+            nbessaie = 0
+
