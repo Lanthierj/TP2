ee3e1fa17753633945e223b9bb250344208db6e1
lanthierj <lanthierj@educ.college-montreal.qc.ca>
2022-10-14 09:05:23 -0400

    commentaire

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
-#James Lanthier 402
-#TP2 jeu de la devinette
+"""James Lanthier 402
+TP2 jeu de la devinette
+Le code permet de jouer a un jeu qui est une devinnete. Tu dois deviner le nombre qui a été choisi par l'ordinateur en le moins d'essaie possible.
+"""
 import random
-
 jouer = True
 chiffre = random.randint(0, 100)
 nbessaie = 0
+def main():
+    """
 
-while jouer:
-    essai = int(input('Entrer votre essaie:'))
+    :return:
+    """
+    while jouer:
+        essai = int(input('Entrer votre essaie:'))
 
-    if essai < chiffre:
-        print('Mauvaise réponse, le nombre est plus grand que', essai)
-        nbessaie += 1
-
-
-    elif essai > chiffre:
-        print('Mauvaise réponse, le nombre est plus petit que', essai)
-        nbessaie += 1
+        if essai < chiffre:
+            print('Mauvaise réponse, le nombre est plus grand que', essai)
+            nbessaie += 1
 
 
-    elif essai == chiffre:
-        print('Bravo! Bonne réponse/Vous avez réussi en : ', nbessaie, 'essaie')
-        restart = input('Voulez-vous faire une autre partie (o,n)?')
+        elif essai > chiffre:
+            print('Mauvaise réponse, le nombre est plus petit que', essai)
+            nbessaie += 1
 
-        if restart == 'n':
-            print('Merci et au revoir... ')
-            jouer = False
 
-        elif restart == 'o':
-            chiffre = random.randint(0, 10)
-            nbessaie = 0
+        elif essai == chiffre:
+            print('Bravo! Bonne réponse/Vous avez réussi en : ', nbessaie, 'essaie')
+            restart = input('Voulez-vous faire une autre partie (o,n)?')
 
+            if restart == 'n':
+                print('Merci et au revoir... ')
+                jouer = False
+
+            elif restart == 'o':
+                chiffre = random.randint(0, 10)
+                nbessaie = 0
+
+main()
